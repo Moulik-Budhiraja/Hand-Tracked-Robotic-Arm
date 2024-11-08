028c62f2e777f30e7578329805d9b489aa542fe8
Xinghao Li <xinghaoli.2006@gmail.com>
2024-11-08 15:00:53 -0500

    added daniel's z calculation

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import cv2
 import numpy as np
-
+import math
 from landmarks import calculate_distance, calculate_world_coordinates
 from mp_hand_tracking import initialize_hand_detector, process_image, hand_connections
 from plotting import plot
 from imutils.video import WebcamVideoStream
 from imutils.video import FPS
+from dataclasses import dataclass
 import imutils
+
+@dataclass
+class FakeLandmark:
+    x: float
+    y: float
+    z: float
+
+def getZ(landmark, offset):
+  return landmark.z + offset;
+
+def getAreaFromLandmarks(landmarks, a, b):
+
+  return math.sqrt((landmarks[a].x - landmarks[b].x)**2 + (landmarks[a].y - landmarks[b].y)**2);
+ 
+def transformScreenLandmarks(landmarks, image):
+  return [FakeLandmark(landmark.x * image.shape[1], landmark.y * image.shape[0], 0) for landmark in landmarks];
+
+def getAmountHandClosed(landmarks):
+  vec1 = (landmarks[5].x - landmarks[0].x, landmarks[5].y - landmarks[0].y, landmarks[5].z - landmarks[0].z);
+  vec2 = (landmarks[6].x - landmarks[5].x, landmarks[6].y - landmarks[5].y, landmarks[6].z - landmarks[5].z);
+
+  return abs((vec1[0] * vec2[0] + vec1[1] * vec2[1] + vec1[2] * vec2[2]) / (math.sqrt(vec1[0] ** 2 + vec1[1] ** 2 + vec1[2] ** 2) * math.sqrt(vec2[0] ** 2 + vec2[1] ** 2 + vec2[2] ** 2)));
+
+
 
 
 
@@ -18,7 +43,10 @@ def main():
 
     frame_counter = 0
     reset = True
+    scale = None
     estimated_distance = None
+
+    data_div = [];
 
     while open_camera:
         image = vs.read() 
@@ -41,6 +69,7 @@ def main():
             world_landmarks = results.multi_hand_world_landmarks[0]
             centre_world = world_landmarks.landmark[0]
 
+            # Initialize reference distances and scale factor
             if reset:
                 reference_distance_vertical = current_distance_vertical
                 reference_distance_horizontal = current_distance_horizontal
@@ -57,6 +86,18 @@ def main():
                 estimated_distance = reference_distance_depth/scale_horizontal - (index_finger_screen.z + pinky_finger_screen.z)/2
             
 
+            world_area = getAreaFromLandmarks(results.multi_hand_world_landmarks[0].landmark, 5, 17) + getAreaFromLandmarks(results.multi_hand_world_landmarks[0].landmark, 0, 9);
+            screen_area = getAreaFromLandmarks(transformScreenLandmarks(results.multi_hand_landmarks[0].landmark, image), 5, 17) + getAreaFromLandmarks(transformScreenLandmarks(results.multi_hand_landmarks[0].landmark, image), 0, 9);
+
+            # World area / screen area ^ 3/4
+            data_div.append(world_area * 1000 / screen_area);
+
+            # data_div[-1] -= data_div[-1] * 0.3 * ((1 - getAmountHandClosed(results.multi_hand_world_landmarks[0].landmark)) ** 2)# - 5 ** (1 - getAmountHandTilted(results.multi_hand_world_landmarks[0].landmark)));
+            data_div[-1] = data_div[-1] * 3 - 0.6
+
+            if (len(data_div) >= 2):
+                data_div[-1] = data_div[-2] * 0.6 + data_div[-1] * 0.4
+
             parts = {
                 "palm": [0, 5, 9, 13, 17, 0],
                 "thumb": [0, 1, 2, 3, 4],
@@ -69,6 +110,10 @@ def main():
             x_data = []
             y_data = []
             z_data = []
+            z_test = []
+
+            # print(hand_landmarks.landmark[0].y, wrist_screen.y, calculate_world_coordinates(centre_world, world_landmarks.landmark[5]).y + wrist_screen.y)
+
 
             for part in parts.values():
                 x_part = []
@@ -85,14 +130,16 @@ def main():
                     y_part.append(scaled_y)
                     z_part.append(scaled_z)
 
+                    
+                z_test.append(list(map(lambda landmark : getZ(landmark, data_div[-1] / 5), [results.multi_hand_world_landmarks[0].landmark[start] for start in part])))
                 x_data.append(x_part)
                 y_data.append(y_part)
                 z_data.append(z_part)
 
 
-
-            plotter.update_scatter_plot(x_data, y_data, z_data)
-            plotter.update_2d_plot(frame_counter, x_data[0][0], y_data[0][0], z_data[0][0])
+           
+            plotter.update_scatter_plot(x_data, y_data, z_test)
+            # plotter.update_2d_plot(frame_counter, x_data[0][0], y_data[0][0], z_data[0][0])
             frame_counter += 1
 
             hand_connections(image, hand_landmarks, results.multi_hand_landmarks)
